7b6ff9abc58ee8d98b24d304f947ceec37d676ef
Maxim <mdolgov322@gmail.com>
2023-05-19 12:31:26 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let tg = winfow.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#FF00FF";
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+	tg.MainButton.setText("Message sended");
+	tg.MainButton.show();
+	tg.sendData("sendTestMessage");
+});
